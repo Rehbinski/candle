81268af4c777af36ef6ef56c85b06bcbdb4cd162
work <Bennirehberg@gmail.com>
2017-06-21 08:39:26 +0200

    Funktion ausgelagert und Sender eingefuegt

--- forensic_function/claimscan.py
+++ forensic_function/claimscan.py
@@ -4,6 +4,9 @@ import subprocess
 import os
 import time
 
+from start_client import sendMessage
+
+
 from forensic_function.global_function import *
 
 def clamscan(directory):
@@ -11,14 +14,32 @@ def clamscan(directory):
     pfad = directory + '/' + commandName
     #clamscan /home/work/NAS/Kunde/usb -r -i --copy=/home/work/NAS/Kunde/clamscan
     command = commandName + ' ' + directory + '/usb' + ' -r -i --copy='  +  pfad
-
     commandListSudoDokumentation(command, directory)
+
+
+def clamscanDisk(directory):
+    print('dritter Schritt begonnen')
+    clamscan(directory)
+    print('dritter Schritt fertig')
+
+    severity = 'info.Windows'  # Nach welchen Kritereien zu Warteschlange geroutet wird
+    exchange = 'topic_logs'  # Wie man mag
+    type = 'topic'  # Type auf welche Art der Worker hört
+    message = 'Hello World'  # Nachricht zum senden erzeugen
+    directory = '/home/work/NAS/Kunde'  # Pfad angabe
+    # Wird Nachricht benötigt???
+    prio = 0  # Priorität festlegen
+
+    sendMessage(exchange, severity, message, directory, prio, type)
+
+
+    print('jaa so gehts')
+
+
 
 
 
 if __name__ == "__main__" :
 
     directory = '/home/work/NAS/Kunde'
-    t = clamscan(directory)
-
-    print('dritter Schritt fertig')
+    clamscanDisk(directory)
